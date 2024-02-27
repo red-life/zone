e752378d511cf8ee67a188cdc1f0fc44e7b3e2e6
Shayan <r3d.l1fe6@gmail.com>
2024-02-27 14:45:15 +0330

    create record model

--- internal/management/models.go
+++ internal/management/models.go
@@ -1,8 +1,32 @@
 package management
 
-import "github.com/google/uuid"
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+)
+
+type RecordType string
+
+const (
+	A     RecordType = "A"
+	AAAA  RecordType = "AAAA"
+	MX    RecordType = "MX"
+	NS    RecordType = "NS"
+	TXT   RecordType = "TXT"
+	CNAME RecordType = "CNAME"
+	PTR   RecordType = "PTR"
+)
 
 type Zone struct {
 	ID   uuid.UUID
 	Zone string
+}
+
+type Record struct {
+	ID     uuid.UUID
+	ZoneID uuid.UUID
+	Name   string
+	Type   RecordType
+	TTL    uint32
+	Value  json.RawMessage
 }
