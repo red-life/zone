ff870ae2eda8df3fa23cd564da8bb149d4ed1be1
Shayan <r3d.l1fe6@gmail.com>
2024-02-27 15:57:54 +0330

    add gorm tags

--- internal/management/models.go
+++ internal/management/models.go
@@ -1,8 +1,8 @@
 package management
 
 import (
-	"encoding/json"
 	"github.com/google/uuid"
+	"github.com/jackc/pgtype"
 )
 
 type RecordType string
@@ -18,15 +18,16 @@ const (
 )
 
 type Zone struct {
-	ID   uuid.UUID
-	Zone string
+	ID   uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Zone string    `gorm:"unique;not null"`
 }
 
 type Record struct {
-	ID     uuid.UUID
-	ZoneID uuid.UUID
-	Name   string
-	Type   RecordType
-	TTL    uint32
-	Value  json.RawMessage
+	ID     uuid.UUID    `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	ZoneID uuid.UUID    `gorm:"type:uuid"`
+	Name   string       `gorm:"type:varchar(255);unique;not null"`
+	Type   RecordType   `gorm:"type:varchar(7);unique;not null"` // not using enums due to further update that might support other record types
+	TTL    uint32       `gorm:"type:integer;check:ttl >= 0;not null"`
+	Value  pgtype.JSONB `gorm:"type:jsonb;not null"`
+	Zone   Zone         `gorm:"references:ZoneID;foreignKey:ZoneID;constraint:OnDelete:CASCADE"`
 }
