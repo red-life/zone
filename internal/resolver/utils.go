c73a16c04faf81ff3cf8a0673fc13e8af817372b
Shayan <r3d.l1fe6@gmail.com>
2024-02-29 12:02:36 +0330

    add string marshalizer to resolver

--- internal/resolver/utils.go
+++ internal/resolver/utils.go
@@ -16,3 +16,19 @@ func MustFromString[T any](s string) T {
 	}
 	return t
 }
+
+func String(data any) (string, error) {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func MustString(data any) string {
+	s, err := String(data)
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
