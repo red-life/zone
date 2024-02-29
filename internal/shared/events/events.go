136cf211cb657687b4db7f08850c105eafcda698
Shayan <r3d.l1fe6@gmail.com>
2024-02-29 09:15:21 +0330

    add events

---
+++ internal/shared/events/events.go
@@ -0,0 +1,44 @@
+package events
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+)
+
+const (
+	ZoneCreatedEvent   = "ZoneCreated"
+	ZoneDeletedEvent   = "ZoneDeleted"
+	RecordAddedEvent   = "RecordAdded"
+	RecordUpdatedEvent = "RecordUpdated"
+	RecordDeletedEvent = "RecordDeleted"
+)
+
+type ZoneCreated struct {
+	ZoneID uuid.UUID `json:"zone_id"`
+	Zone   string    `json:"zone"`
+}
+
+type ZoneDeleted struct {
+	ZoneID uuid.UUID `json:"zone_id"`
+}
+
+type RecordAdded struct {
+	ZoneID   uuid.UUID       `json:"zone_id"`
+	RecordID uuid.UUID       `json:"record_id"`
+	Name     string          `json:"name"`
+	TTL      uint32          `json:"ttl"`
+	Value    json.RawMessage `json:"value"`
+}
+
+type RecordUpdated struct {
+	ZoneID   uuid.UUID       `json:"zone_id"`
+	RecordID uuid.UUID       `json:"record_id"`
+	Name     string          `json:"name"`
+	TTL      uint32          `json:"ttl"`
+	Value    json.RawMessage `json:"value"`
+}
+
+type RecordDeleted struct {
+	ZoneID   uuid.UUID `json:"zone_id"`
+	RecordID uuid.UUID `json:"record_id"`
+}
