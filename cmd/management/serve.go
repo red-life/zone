e0cd545d66e3c93404a3ff95a46d3339a5e175bd
Shayan <r3d.l1fe6@gmail.com>
2024-02-29 08:13:01 +0330

    provide message queue to management service using redis

--- cmd/management/serve.go
+++ cmd/management/serve.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/red-life/zone/cmd"
 	"github.com/red-life/zone/internal/management"
 	"github.com/red-life/zone/internal/management/adapters/http"
 	postgresRepo "github.com/red-life/zone/internal/management/adapters/postgres"
+	"github.com/red-life/zone/pkg/mq"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"time"
 )
 
-func main() {
-	cfg := cmd.ParseManagementConfig()
+func newGormDB(postgresCfg cmd.Postgres) *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
-		cfg.Postgres.Host, cfg.Postgres.Username, cfg.Postgres.Password,
-		cfg.Postgres.DB, cfg.Postgres.Port)
+		postgresCfg.Host, postgresCfg.Username, postgresCfg.Password,
+		postgresCfg.DB, postgresCfg.Port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		TranslateError: true,
 	})
@@ -28,8 +31,31 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to ping db: %s\n", err)
 	}
+	return db
+}
+
+func newRedisClient(redisCfg cmd.Redis) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisCfg.Addr,
+		Password: redisCfg.Password,
+		DB:       redisCfg.DB,
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err := rdb.Ping(ctx).Err()
+	if err != nil {
+		log.Fatalf("failed to ping redis: %s", err)
+	}
+	return rdb
+}
+
+func main() {
+	cfg := cmd.ParseManagementConfig()
+	db := newGormDB(cfg.Postgres)
 	repo := postgresRepo.NewManagementRepository(db)
-	service := management.NewManagementService(repo)
+	rdb := newRedisClient(cfg.Redis)
+	redisMQ := mq.NewRedisMessageQueue(rdb)
+	service := management.NewManagementService(repo, redisMQ)
 	api := http.NewAPI(service)
 	engine := gin.Default()
 	authMiddleware := http.BasicAuth(cfg.Username, cfg.Password)
